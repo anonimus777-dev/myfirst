7067f99cea5e22768e6d3dd5d361118f5f0f8d4d
anonimus777-dev <anonchiks007@gmail.com>
2026-08-25 10:57:05 +0500

    Update общие.py

--- клавиатуры/общие.py
+++ клавиатуры/общие.py
@@ -1,1 +1,16 @@
 """Общие и информационные клавиатуры."""
+# -*- coding: utf-8 -*-
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def no_bunker_keyboard():
+    return InlineKeyboardMarkup([[InlineKeyboardButton("🏗 Создать бункер", callback_data="create_bunker")]])
+
+def back_keyboard():
+    return InlineKeyboardMarkup([[InlineKeyboardButton("◀️ Назад", callback_data="help_back")]])
+
+def donate_keyboard():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("Купить 🪙 BBCoins", url="https://t.me/Alkyrin")],
+        [InlineKeyboardButton("💰 Сайт для доната 💰", url="https://t.me/Alkyrin")],
+    ])
