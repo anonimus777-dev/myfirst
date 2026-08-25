c65f91e9b897f2b35cf51fb84a18190c0a18c152
anonimus777-dev <anonchiks007@gmail.com>
2026-08-25 10:56:56 +0500

    Update главное_меню.py

--- клавиатуры/главное_меню.py
+++ клавиатуры/главное_меню.py
@@ -1,1 +1,12 @@
 """Главное меню бота."""
+# -*- coding: utf-8 -*-
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def help_main_keyboard():
+    return InlineKeyboardMarkup([
+        [InlineKeyboardButton("💡 Основное", callback_data="help_main"),
+         InlineKeyboardButton("🎲 Игры", callback_data="help_games")],
+        [InlineKeyboardButton("💥 Активности", callback_data="help_activities"),
+         InlineKeyboardButton("💬 Чаты", callback_data="help_chats")],
+    ])
